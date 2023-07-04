8b142ded6776465cd8e18852e3a7830f1ebc4086
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-07-04 16:30:24 +0300

    adding api and jwt

--- app.js
+++ app.js
@@ -10,19 +10,48 @@ app.use(express.urlencoded({ extended: false}))
 
 app.set('view engine', 'ejs')
 
+let user = {
+  id:"sgaasa",
+  email:"alice@gmail.com",
+  password:"dshaaaahaaha;'agasaaaagaga"
+}
+
+//because not a production app just for learning i will not make a env or db
+
+const JWT_SECRET = 'some super secret...'
+
 app.get('/',(req,res)=>{
   res.send("Hello World")
 })
 
 app.get('/forget-password',(req,res,next)=>{
-
+res.render('forgot-password')
 })
 
 app.post('/forget-password',(req,res,next)=>{
+const {email} = req.body
 
+//make sure this the user exist in database
+
+if(email !== user.email){
+  res.send('User not registered')
+  return
+}
+
+const secret = JWT_SECRET = user.password
+
+const payload = {
+  email:user.email,
+  id:user.id
+}
+
+const token = jwt.sign(payload, secret,{expiresIn:'15m'})
+const link = `http://localhost:3000/reset-password/${user.id}/${token}`
+console.log(link)
+res.send('Passsword rest link has been sent to your email')
 })
 
-app.get('/reset-password',(req,res,next)=>{
+app.get('/reset-password/',(req,res,next)=>{
 
 })
 app.post('/reset-password',(req,res,next)=>{
